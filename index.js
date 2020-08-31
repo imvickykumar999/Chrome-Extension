e90eed6cbd4590cfca21251b2eb84fa3ee429221
Vicky Kumar <imvickykumar999@gmail.com>
2020-08-31 14:01:27 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const iframeContainer = document.querySelector(".container");
-function widthAndHeightManager(){
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    iframeContainer.width = width + "px";
-    iframeContainer.height = height - 28 + "px";
-}
-widthAndHeightManager();
-window.addEventListener("resize",widthAndHeightManager);
